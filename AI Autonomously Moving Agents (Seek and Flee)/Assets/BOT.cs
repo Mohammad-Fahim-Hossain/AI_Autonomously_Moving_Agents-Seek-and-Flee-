1f36e50b6eb28995142259be871bce5b8cda339c
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2020-07-31 13:41:38 +0600

    Upload the project.

---
+++ AI Autonomously Moving Agents (Seek and Flee)/Assets/BOT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BOT : MonoBehaviour
+{
+    public GameObject Target;
+    NavMeshAgent Agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Agent = this.GetComponent<NavMeshAgent>(); 
+    }
+
+    void Seek(Vector3 Location) {
+
+        Agent.SetDestination(Location);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Seek(Target.transform.position);
+    }
+}
